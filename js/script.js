53b06b66c4343aac04d3a7dc289722156c71a3f9
Almiron Emiliano <almironemiliano@gmail.com>
2025-06-11 19:28:44 -0300

    agregamos clases al nav

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
-const toggleButton = document.querySelector('.menu-toggle');
-const nav = document.getElementById('nav-links');
+document.addEventListener("DOMContentLoaded", () => {
+    const toggleButton = document.querySelector(".menu-toggle");
+    const nav = document.getElementById("nav-links");
 
-toggleButton.addEventListener('click', () => {
-  nav.classList.toggle('open');
+    toggleButton.addEventListener("click", () => {
+        nav.classList.toggle("active");
+    });
 });
