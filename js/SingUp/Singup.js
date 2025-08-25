d2c4611c6a09d4b35e7b649554fd563e84d005ab
julieta-turco <julivickiturco23@gmail.com>
2025-08-25 00:36:20 -0300

    JS con sus carpetas y html con sus script

--- js/SingUp/Singup.js
+++ js/SingUp/Singup.js
@@ -5,8 +5,32 @@ class UserCreate {
         this.text = "";
         this.password = "";
     }
-}
     getNode = () => {
         const usercreate = document.createElement("section");
         usercreate.className = "box-section";
         usercreate.innerHTML = `
+        <div class="input-group">
+        <i class="fas fa-user" aria-hidden="true"></i>
+        <input type="text" name="username" placeholder="Nombre Completo">
+        </div>
+        <div class="input-group">
+        <i class="fas fa-envelope" aria-hidden="true"></i>
+        <input type="email" name="email" placeholder="Correo Electrónico">
+        </div>
+        <div class="input-group">
+        <i class="fas fa-user" aria-hidden="true"></i>
+        <input type="text" name="nickname" placeholder="Nombre de Usuario">
+        </div>
+        <div class="input-group">
+        <i class="fas fa-lock" aria-hidden="true"></i>
+        <input type="password" name="password" placeholder="Contraseña">
+        </div>
+        <a href="../index.html" class="btn">Registrarse</a>
+        </form>
+        `;  
+        return userCreate;
+    }
+}
+                    
+      
+
