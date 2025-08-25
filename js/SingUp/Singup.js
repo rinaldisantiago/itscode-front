d39b3c869b6e373844ef66fdf26f94a3ffe564c9
julieta-turco <julivickiturco23@gmail.com>
2025-08-25 00:16:03 -0300

    agrego a signup.js el contructor y creo un nodo

---
+++ js/SingUp/Singup.js
@@ -0,0 +1,12 @@
+class UserCreate {
+    constructor() {
+        this.text = "";
+        this.email = "";
+        this.text = "";
+        this.password = "";
+    }
+}
+    getNode = () => {
+        const usercreate = document.createElement("section");
+        usercreate.className = "box-section";
+        usercreate.innerHTML = `
